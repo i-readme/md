4150a09e47de2ac7571665527198e722faeb6ca7
KatieLisabeth <kat.lisabeth@gmail.com>
2024-08-29 18:12:38 +0200

    install sgv reader and adjust settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
+// next.config.mjs
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgo: false, // Disable SVGO
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
